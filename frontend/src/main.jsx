e2eee90a85d5663cc478c8e7896b21728591edde
raakdgol <rotimiakinyemii@gmail.com>
2025-03-26 13:23:55 +0100

    Improve frontend layout: add global fonts, integrate ShopContextProvider, and include Hero & LatestCollection on Home page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import ShopContextProvider from './context/ShopContext.jsx'
 
 
 // createRoot(document.getElementById('root')).render(
-  ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App />
+    <ShopContextProvider>
+      <App/>
+    </ShopContextProvider>
+    
   </BrowserRouter>,
 )
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Hero from '../components/Hero'
+import LatestCollection from '../components/LatestCollection'
 
 const home = () => {
   return (
-    <div></div>
+    <div>
+      <Hero/>
+      <LatestCollection/>
+    </div>
   )
 }
 
